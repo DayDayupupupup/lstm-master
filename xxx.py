8dd6ccc8f8615756feb19b0cef5f58cb6eaf5abc
binbin <841954111@qq.com>
2018-09-10 21:46:20 +0800

    nothing

--- xxx.py
+++ xxx.py
@@ -1,5 +1,12 @@
-arr = input("")
-num = [int(n) for n in arr.split()]
-print(num)
-
-
+'''
+m,n = input().split()
+v = []
+for i in range(0,int(m)):
+    x = input().split()
+    v.append(x)
+print(v)
+'''
+if "abcd" < "dcba":
+    print("dui")
+else:
+    print("xxxx")
