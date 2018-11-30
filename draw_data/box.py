20d052c3b053de79bf7cc2885aff3b0b0e467bed
binbin <841954111@qq.com>
2018-11-30 20:20:41 +0800

    finish 初步

---
+++ draw_data/box.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib
+np.random.seed(2)  #设置随机种子
+zhfont1 = matplotlib.font_manager.FontProperties(fname='C:\Windows\Fonts\simsun.ttc')
+data = [[0.3,0.2,0.2,0.25],
+        [0.6,0.5,0.5,0.6],
+        [0.95,0.47,1.3,0.7],
+        [1.2,0.9,2.0,0.4],
+        [1.8,0.7,2.9,0.8]]
+
+df = pd.DataFrame((data),columns=['LmsLoc', 'PDR', 'PF', 'WIFI'])
+df.boxplot() #也可用plot.box()
+plt.ylabel('定位时间（s）',fontproperties=zhfont1)
+plt.show()
